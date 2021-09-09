e255d9d69516eba1ed3b5353c2e21842372e31b3
matthew chimenti <matthewosu@gmail.com>
2021-09-09 14:33:01 -0700

    non router header made for visual layout

--- src/components/moduels/header.js
+++ src/components/moduels/header.js
@@ -5,8 +5,19 @@ export default function Header() {
     return(
         <>
         <div id="headerContainer">
-            <img id="logo" src={Logo}></img>
-            <p id="headerName">NATURE TO THRIVE</p>
+            <div id="headerLeft">
+                <img id="logo" src={Logo}></img>
+                <p id="headerName">NATURE TO THRIVE</p>
+            </div>
+            <div id="headerMiddle">
+                <p>ABOUT</p>
+                <p>OUTDOOR-ECOTHERAPY</p>
+                <p>PSYCHOTHERAPY</p>
+            </div>
+            <div id="headerRight">
+                <p>FAQ</p>
+                <p>CONTACT</p>
+            </div>
         </div>
         </>
     )
