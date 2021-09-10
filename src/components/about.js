44b59459a9e50b5a85bcacaf2f7374f94679985a
matthew chimenti <matthewosu@gmail.com>
2021-09-09 18:15:33 -0700

    started working on about page

--- src/components/about.js
+++ src/components/about.js
@@ -1,12 +1,23 @@
 import React from 'react'
 import Header from './header.js'
+import Monika from './images/MonikaHeadshot.jpg'
+import MonikaHorizontal from './images/MonikaH.jpg'
 
 export default function AboutPage() {
     return(
         <>
             <Header />
             <div id="aboutPage">
-                <p id="aboutPageTitle">ABOUT MONIKA</p>
+                <div id="aboutPageContextArea"> 
+                    <p id="aboutPageTitle">ABOUT MONIKA</p>
+                    <div>
+                        <img id ="MonikaAboutImg" src={MonikaHorizontal}></img>
+                        <div id="aboutPageContentContainer">
+
+                            <p>SDLKFJSDLFSF SLDKFSDLFK Sflknfsdlkf asdlf sad;fl nasf asd;lf kajsdf l;as a;jfsadasfsadf ksadkfsadf saf asdk;f hsadkjfhd skjfhasdkf sadkf jasdkfj hasdkf hasdk;f hasdkfj asdkf jasd kfjas dfk;jas df ;ksajd</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </>
     )
